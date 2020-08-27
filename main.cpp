3294f7d4c6429cf90ac41f2febbbe3cb103c304d
jaccen <jaccen2007@163.com>
2020-08-27 16:42:24 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//
+
+#include <unistd.h>
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv;
+
+int main( void ){
+    
+    // note finished
+    fprintf(stderr, "\nEmpty project.\n\n");
+    
+    // end
+    return(0);
+}
